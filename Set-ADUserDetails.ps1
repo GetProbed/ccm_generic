cb48f03be66cb53422cf8769b1a9552905b4f294
GetProbed <jim_nap@hotmail.com>
2025-06-06 14:28:51 +1000

    Update Set-ADUserDetails.ps1

--- Set-ADUserDetails.ps1
+++ Set-ADUserDetails.ps1
@@ -49,35 +49,18 @@ function Show-UpdateForm {
     $form.Size = '800,500'
     $form.StartPosition = "CenterScreen"
 
+    # Field mappings
+    $labels = "Address","Phone","Post Code","State","Suburb","Department","Job Title"
+    $props  = "StreetAddress","TelephoneNumber","PostalCode","State","City","Department","Title"
+    $textBoxes = @{}
+
     # Left Panel - User List
     $userList = New-Object Windows.Forms.CheckedListBox
     $userList.Location = '10,10'
     $userList.Size = '300,400'
     $form.Controls.Add($userList)
 
-    # Search box
-    $searchBox = New-Object Windows.Forms.TextBox
-    $searchBox.Location = '10,420'
-    $searchBox.Size = '200,25'
-    $form.Controls.Add($searchBox)
-
-    $searchBtn = New-Object Windows.Forms.Button
-    $searchBtn.Text = "Search"
-    $searchBtn.Location = '220,420'
-    $searchBtn.Add_Click({
-        $userList.Items.Clear()
-        $users = Get-ADUser -Filter "Name -like '*$($searchBox.Text)*'" -Properties * -Server $dc
-        $global:selectedObjects = @()
-        foreach ($user in $users) {
-            $userList.Items.Add($user, $false)
-        }
-    })
-    $form.Controls.Add($searchBtn)
-
     # Right Panel - Fields
-    $labels = "Address","Phone","Post Code","State","Suburb","Department","Job Title"
-    $props  = "StreetAddress","TelephoneNumber","PostalCode","State","City","Department","Title"
-    $textBoxes = @{}
     for ($i = 0; $i -lt $labels.Length; $i++) {
         $lbl = New-Object Windows.Forms.Label
         $lbl.Text = $labels[$i]
@@ -92,6 +75,44 @@ function Show-UpdateForm {
 
         $textBoxes[$props[$i]] = $txt
     }
+
+    # Search box
+    $searchBox = New-Object Windows.Forms.TextBox
+    $searchBox.Location = '10,420'
+    $searchBox.Size = '200,25'
+    $form.Controls.Add($searchBox)
+
+    $searchBtn = New-Object Windows.Forms.Button
+    $searchBtn.Text = "Search"
+    $searchBtn.Location = '220,420'
+    $searchBtn.Add_Click({
+        $userList.Items.Clear()
+        try {
+            $users = Get-ADUser -Filter "Name -like '*$($searchBox.Text)*'" -Properties * -Server $dc
+            foreach ($user in $users) {
+                $userList.Items.Add($user, $false)
+            }
+        } catch {
+            [Windows.Forms.MessageBox]::Show("Search failed: $_")
+        }
+    })
+    $form.Controls.Add($searchBtn)
+
+    # Auto-fill on item check
+    $userList.Add_ItemCheck({
+        Start-Sleep -Milliseconds 100 # Let .CheckedItems update
+        $checked = @()
+        foreach ($item in $userList.CheckedItems) { $checked += $item }
+
+        if ($checked.Count -eq 1) {
+            $u = Get-ADUser -Identity $checked[0].SamAccountName -Properties * -Server $dc
+            foreach ($key in $textBoxes.Keys) {
+                $textBoxes[$key].Text = $u.$key
+            }
+        } elseif ($checked.Count -gt 1) {
+            foreach ($tb in $textBoxes.Values) { $tb.Text = "" }
+        }
+    })
 
     # Apply Button
     $applyBtn = New-Object Windows.Forms.Button
@@ -116,20 +137,24 @@ function Show-UpdateForm {
         }
 
         foreach ($user in $targets) {
-            $original = Get-ADUser -Identity $user.SamAccountName -Properties * -Server $dc
-            foreach ($key in $sharedProps.Keys) {
-                $old = $original.$key
-                $new = $sharedProps[$key]
-                if ($old -ne $new) {
-                    try {
-                        if (-not $dryRun) {
-                            Set-ADUser -Identity $original -Replace @{ $key = $new } -Server $dc
+            try {
+                $original = Get-ADUser -Identity $user.SamAccountName -Properties * -Server $dc
+                foreach ($key in $sharedProps.Keys) {
+                    $old = $original.$key
+                    $new = $sharedProps[$key]
+                    if ($old -ne $new) {
+                        try {
+                            if (-not $dryRun) {
+                                Set-ADUser -Identity $original -Replace @{ $key = $new } -Server $dc
+                            }
+                            Log-Change -User $user.SamAccountName -Field $key -Old $old -New $new -Status "Success"
+                        } catch {
+                            Log-Change -User $user.SamAccountName -Field $key -Old $old -New $new -Status "Failed: $_"
                         }
-                        Log-Change -User $user.SamAccountName -Field $key -Old $old -New $new -Status "Success"
-                    } catch {
-                        Log-Change -User $user.SamAccountName -Field $key -Old $old -New $new -Status "Failed: $_"
                     }
                 }
+            } catch {
+                Log-Change -User $user.SamAccountName -Field "N/A" -Old "N/A" -New "N/A" -Status "Error loading user: $_"
             }
         }
 
@@ -147,5 +172,4 @@ $dc = Select-DomainController
 if (-not $dc) { return }
 
 "User,Field,Old,New,Status" | Out-File -FilePath $logPath -Encoding UTF8
-
 Show-UpdateForm -dc $dc
